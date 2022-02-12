8c0b2e9908319026c9b8907dab40ccbfc15b16ff
Brad Hodge <bhodge166@gmail.com>
2022-02-11 19:08:16 -0700

    Wrote readme explaining code. Added comments to js code to explain functionality

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # homework3_pwg
+This code is for a password generator. It includes prompts to determine how many characters the user wants their password to be and if they would like to include lower case, upper case, numbers, or special characters. It outputs a randomized password to the users specification. 
+
+The code works by creating an array for each of the options above, adding a random amount from each based on the users selected characters to a new array, and then using a while loop to fill any missing characters. 
+
+A shuffling fuction found on stackoverflow was added at the end to randomize the created array.
+
+Screenshot of finished work can be seen below:
+
+![Homework Screenshot](./assets/images/Homework3Screenshot.png)
+
+The website is deployed here:
+https://bhodge166.github.io/homework3_pwg/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// variables for option arrays
 var letters = "abcdefghijklmnopqrstuvwxyz".split("");
 var numbers = "1234567890".split("")
 var upperletters = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
 var upperArray = upperletters.split("");
 var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
 
-
+// variable to store randomly generated characters from arrays above
 var generatedPassword = [];
-
+// variables to capture inputs from prompts
 var lowerCon = 0;
 var upperCon = 0;
 var specialCon = 0;
@@ -19,6 +19,7 @@ var confirmAmount = 0;
 
 // Write password to the #password input
 function writePassword() {
+  // to reset on new click
   password = ""
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -27,15 +28,18 @@ function writePassword() {
 }
 
 function generatePassword() {
+  // to reset on new click
   lowerCon = 0;
   upperCon = 0;
   specialCon = 0;
   numbersCon = 0;
   categories = 0;
   generatedPassword = []
+  // user prompts to determine password specifications
   confirmAmount = window.prompt(
     "Please input desired password length between 8 and 128 characters:"
   );
+  // if else to confirm password is within guidelines
   if (confirmAmount < 8 || confirmAmount > 128) {
     window.alert("Password length must be between 8 and 128 characters");
   } else {
@@ -50,6 +54,7 @@ function generatePassword() {
       "Would you like to include special characters?"
     );
   }
+  // increase variables above if user wishes to include. used below to determine if functions to randomly select characters run below
   if (confirmLower === true) {
     lowerCon++;
   }
@@ -62,8 +67,10 @@ function generatePassword() {
   if (confirmSpecial === true) {
     specialCon++;
   }
+  // sets amount of categories based on user criteria. used so each selection gets at least 1 character
   categories = lowerCon + upperCon + specialCon + numbersCon;
  
+  // runs functions if user chooses to include
   if (lowerCon > 0) {
    generateLowercaseLetters();}
  
@@ -78,16 +85,19 @@ function generatePassword() {
   if (specialCon > 0) {
     generateSpecial()
   }
-
+// adds additional characters to generated password up to users selected amount by pulling random characters from the created array
   while (generatedPassword.length < confirmAmount) {
     generatedPassword.push(generatedPassword[Math.floor(Math.random() * generatedPassword.length)])
   }
-
+// shuffles completed password finished above
   shufflePassword(generatedPassword)
 
+  // returns completed password
   return generatedPassword.join("");
 }
 
+// functions to pull from option arrays randomly and push into new array a random number of times based on categories selected and remaining password lengths
+// 1 is added at the end of result to ensure that there is at least 1 character chosen from selection
 function generateLowercaseLetters() {
   categories--;
   var result = Math.floor(
@@ -148,5 +158,5 @@ function shufflePassword(generatedPassword) {
   return generatedPassword
 }
 
-// Add event listener to generate button
+// Given event listener to generate button
 generateBtn.addEventListener("click", writePassword);
